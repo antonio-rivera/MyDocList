be1aa33e8d1125baaa63290114443f0ea4be735c
Antonio Rivera <antoniorivera.cs18@gmail.com>
2020-10-23 12:10:44 -0400

    Project will go public

--- server/db/index.js
+++ server/db/index.js
@@ -1,17 +1,21 @@
 //Establish connection with our cloud mongo server
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 //Set debugger to monitor CRUD operations
-mongoose.set('debug', true);
+mongoose.set("debug", true);
 
-
-const connectionString = 'mongodb+srv://currentUser:7E62nph4uXiRTeC@cluster0.ia1fn.mongodb.net/DocDB?retryWrites=true&w=majority'
+//Ask me for the connection string. Email me at antoniorivera.cs18@gmail.com
+const connectionString = "";
 
 //Connect database to our mongo server, default port and db name passed to the URI
-mongoose.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true}) //if not created, create and connect
-.catch(e => {
-    console.error('Connection error: ', e.message)
-})
+mongoose
+  .connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }) //if not created, create and connect
+  .catch((e) => {
+    console.error("Connection error: ", e.message);
+  });
 //Error is thrown if connection is unsuccessful
 
 //cursor to access mongo database
